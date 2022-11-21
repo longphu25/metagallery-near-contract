ea68c93a1491d3100e9cbbd409c873608abeb42b
Long Phu <phu.huynh@monimedia.com>
2022-11-21 16:13:04 +0000

    Update Contract NFT

---
+++ nft-contract/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
+# cargo build --all --target wasm32-unknown-unknown --release
+mkdir -p out
+cp target/wasm32-unknown-unknown/release/*.wasm ./out/
